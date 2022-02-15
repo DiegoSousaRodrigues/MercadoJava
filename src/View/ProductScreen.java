7ad45df0d4d7ec1606c7727d4aaee63c9e931d6a
DiegoSousaRodrigues2 <80047822+DiegoSousaRodrigues2@users.noreply.github.com>
2022-02-15 20:16:43 -0300

    mais uns passos

---
+++ src/View/ProductScreen.java
@@ -0,0 +1,14 @@
+package View;
+
+import javax.swing.*;
+import java.awt.event.ActionListener;
+
+public class ProductScreen{
+
+    public ActionListener AddScreenAddProduto(){
+        JFrame tela = new JFrame();
+
+
+        return null ;
+    }
+}
--- src/View/Screen.java
+++ src/View/Screen.java
@@ -2,20 +2,41 @@ package View;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.Locale;
 
 public class Screen {
-    JFrame tela = new JFrame();
-    private JButton ok;
+    private JFrame tela = new JFrame();
+    private JButton addProd, closeJframe;
+    private ProductScreen prdScreen = new ProductScreen();
 
     public void mainScreen() {
         tela.setSize(500, 800);
 
-        ok = new JButton("Adicionar Produto");
+        addProd = new JButton("Adicionar Produto");
         tela.setLayout(null);
-        ok.setBounds(200,400,150,60);
-        tela.add(ok);
+        addProd.setBounds(160,200,150,60);
+        tela.add(addProd);
 
+        closeJframe = new JButton("Sair");
+        tela.setLayout(null);
+        closeJframe.setBounds(160, 300,150,60);
+        tela.add(closeJframe);
+
+        closeJframe.addActionListener(e ->{
+            tela.dispose();
+        });
+        addProd.addActionListener(e -> {
+            tela.setVisible(false);
+            prdScreen.AddScreenAddProduto();
+            tela.setVisible(true);
+        });
+
+        tela.setLocationRelativeTo(null);
         tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         tela.setVisible(true);
+
     }
+
+
 }
